827abf3b5371b94c0783b6592a473accf8d2fde3
PZ <pzika@langmaster.cz>
2017-02-05 15:37:31 +0100

    v

---
+++ index.js
@@ -0,0 +1,14 @@
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var init;
+    return {
+        setters: [],
+        execute: function () {
+            exports_1("init", init = function () {
+                alert('Hallo');
+            });
+        }
+    };
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7O1lBQUEsa0JBQWEsSUFBSSxHQUFHO2dCQUNsQixLQUFLLENBQUMsT0FBTyxDQUFDLENBQUM7WUFDakIsQ0FBQyxFQUFBO1FBQUEsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBjb25zdCBpbml0ID0gKCkgPT4ge1xyXG4gIGFsZXJ0KCdIYWxsbycpO1xyXG59Il19
