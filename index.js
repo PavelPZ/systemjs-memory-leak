c943125598a3478489fca8dd542af59e9bc6520b
PZ <pzika@langmaster.cz>
2017-02-05 17:01:38 +0100

    v

--- index.js
+++ index.js
@@ -9,18 +9,21 @@ System.register(["./library.js"], function (exports_1, context_1) {
         ],
         execute: function () {
             exports_1("init", init = function (withLibrary) {
+                //correct page title
                 document.getElementById('title').innerHTML = 'Used ' + (withLibrary ? 'with library' : 'without library');
+                //hide buttons - use Refresh page display them again
                 document.getElementById('buttons').innerHTML = '';
+                //infinite IMPORT - DELETE loop
                 loadLazies(withLibrary);
             });
             count = 0;
             loadLazies = function (withLibrary) {
-                //load and delete 
-                // - or lazy-with-lib.js (which depends library.js) 
+                //load and delete: 
+                // - or lazy-with-lib.js (which depends on library.js) 
                 // - or lazy-without-lib (without any dependencies)
                 var id = withLibrary ? 'lazy-with-lib' : 'lazy-without-lib';
                 id = id + '.js?' + count.toString();
-                //infinite loop: import and delete selected lazy???.js 
+                //infinite loop: import and delete lazy??.js 
                 System.import(id).then(function (m) {
                     m.init(count++);
                     System.delete(System.normalizeSync(id));
--- index.ts
+++ index.ts
@@ -1,20 +1,23 @@
 ﻿import './library.js'; //force import library.js
 
 export const init = (withLibrary: boolean) => {
+  //correct page title
   document.getElementById('title').innerHTML = 'Used ' + (withLibrary ? 'with library' : 'without library');
+  //hide buttons - use Refresh page display them again
   document.getElementById('buttons').innerHTML = '';
+  //infinite IMPORT - DELETE loop
   loadLazies(withLibrary);
 }
 
 let count = 0;
 const loadLazies = (withLibrary: boolean) => {
-  //load and delete 
-  // - or lazy-with-lib.js (which depends library.js) 
+  //load and delete: 
+  // - or lazy-with-lib.js (which depends on library.js) 
   // - or lazy-without-lib (without any dependencies)
   let id = withLibrary ? 'lazy-with-lib' : 'lazy-without-lib';
   id = id + '.js?' + count.toString();
 
-  //infinite loop: import and delete selected lazy???.js 
+  //infinite loop: import and delete lazy??.js 
   System.import(id).then(m => {
     m.init(count++);
     System.delete(System.normalizeSync(id));
